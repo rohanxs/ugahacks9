35a4c23d6a71fab8c86d71758e19c279fa14a865
Rohan Singh <rohansingh07294@gmail.com>
2024-02-10 03:24:21 -0500

    Preferences transition

--- DawgEats/app.js
+++ DawgEats/app.js
@@ -3,3 +3,15 @@ const signOutButton = document.getElementById("signout");
 signOutButton.onclick = () => {
   window.location.href = "signin.html";
 }
+
+const browseButton = document.getElementById("browse");
+
+browseButton.onclick = () => {
+  // impl later
+}
+
+const preferencesButton = document.getElementById("preferences");
+
+preferencesButton.onclick = () => {
+  window.location.href = "preferences.html";
+}
