112c10598742df29f52ae92b1b6a29b3d7ba72b1
Rohan Singh <rohansingh07294@gmail.com>
2024-02-10 04:54:05 -0500

    Begin scripting of oAuth2 Firebase data retrieval

---
+++ DawgEats/preferences.js
@@ -0,0 +1,16 @@
+let vegetarian = document.getElementById("c1").checked;
+let vegan = document.getElementById("c2").checked;
+let noEggs = document.getElementById("c3").checked;
+let noWheat = document.getElementById("c4").checked;
+let noGluten = document.getElementById("c5").checked;
+let protein = document.getElementById("c6").checked;
+let noLactose = document.getElementById("c7").checked;
+let loseWeight = document.getElementById("c8").checked;
+let noCholestrol = document.getElementById("c9").checked;
+let noCalories = document.getElementById("c10").checked;
+let noSodium = document.getElementById("c11").checked;
+
+const nosaveButton = document.getElementById("nosave");
+nosaveButton.onclick = () => {
+  window.location.href = "app.html"
+}
