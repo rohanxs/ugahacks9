9087bef8fb9c4c7d23d2e6e62eb2d89efb8a6d36
Rohan Singh <rohansingh07294@gmail.com>
2024-02-11 05:35:34 -0500

    More registration debugging

--- DawgEats/signin.js
+++ DawgEats/signin.js
@@ -1,3 +1,37 @@
+function login(email, password) {
+  const request = require('request');
+
+  request.get({
+    url: 'https://localhost/profile',
+    auth: {
+      username: email,
+      password: password
+    }
+    // Auth0 API with simple request to hosting URL
+  }, function(err, response, body) {
+    if (err) return unrecognizedCredentials();
+    if (response.statusCode === 401) return unrecognizedCredentials();
+    const user = JSON.parse(body);
+
+    userAuthenticated();
+  });
+};
+
+const signinButton = document.getElementById("signin");
+const registerButton = document.getElementById("register");
+const usernameField = document.getElementById("username");
+const passwordField = document.getElementById("password");
+
+signinButton.onclick = () => {
+  let username = usernameField.value.toString();
+
+  // oAuth2 login.
+  login(
+    username, 
+    passwordField.value.toString()
+  )
+}
+
 function unrecognizedCredentials() {
   window.location.href = "signin.html";
   return false;
@@ -7,11 +41,6 @@ function userAuthenticated() {
   window.location.href = "app.html";
   return true;
 }
-
-const signinButton = document.getElementById("signin");
-const registerButton = document.getElementById("register");
-const usernameField = document.getElementById("username");
-const passwordField = document.getElementById("password");
 
 signinButton.onclick = () => {
   let username = usernameField.value.toString();
@@ -36,40 +65,3 @@ registerButton.onclick = () => {
     window.location.href = "signin.html";
   }
 }
-
-/*
-function login(email, password) {
-  const request = require('request');
-
-  request.get({
-    url: 'https://localhost/profile',
-    auth: {
-      username: email,
-      password: password
-    }
-    //for more options check:
-    //https://github.com/mikeal/request#requestoptions-callback
-  }, function(err, response, body) {
-    if (err) return unrecognizedCredentials();
-    if (response.statusCode === 401) return unrecognizedCredentials();
-    const user = JSON.parse(body);
-
-    userAuthenticated();
-  });
-};
-
-// get details from input fields
-const signinButton = document.getElementById("signin");
-const usernameField = document.getElementById("username");
-const passwordField = document.getElementById("password");
-
-signinButton.onclick = () => {
-  let username = usernameField.value.toString();
-
-  // oAuth2 login.
-  login(
-    username, 
-    passwordField.value.toString()
-  )
-}
-*/
